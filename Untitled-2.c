e1e6172136a6d611641e700bafc84a2faea40a74
WSR-Learning <wsr_2025.6.22@qq.com>
2025-11-06 09:03:08 +0800

    Add files via upload

--- Untitled-2.c
+++ Untitled-2.c
@@ -1,28 +1,41 @@
 #include<stdio.h>
 int main(){
-    char a[100];
-    int i=(-1),b=0,c=0,d=0,e=0;
-    gets(a);
-    while(1){
-        i=i+1;
-        if(a[i]=='\0'){
-            break;
+    int i,j,n;
+    scanf("%d",&n);
+    char r[2*n-1][2*n-1];
+    for(i=0;i<=(n-1);i=i+1){
+        for(j=(n-i-1);j<=(n+i-1);j=j+1){
+            r[i][j]='*';
         }
-        else{
-            if((a[i]>='A' && a[i]<='Z') || (a[i]>='a' && a[i]<='z')){
-            b=b+1;
-            }
-            else if(a[i]==' '){
-                c=c+1;
-            }
-            else if(a[i]>='0' && a[i]<='9'){
-                d=d+1;
-            }
-            else{
-                e=e+1;
+        if(i<=(n-2)){
+        for(j=0;j<=(n-i-2);j=j+1){
+            r[i][j]=' ';
+        }
+        }
+    }
+    for(i=n;i<=(2*n-2);i=i+1){
+        for(j=(i-n+1);j<=(3*n-i-3);j=j+1){
+            r[i][j]='*';
+        }
+        for(j=0;j<=(i-n);j=j+1){
+            r[i][j]=' ';
+        }
+    }
+    for(i=0;i<=(n-1);i=i+1){
+        for(j=0;j<=(n+i-1);j=j+1){
+            printf("%c",r[i][j]);
+            if(j==(n+i-1)){
+                printf("\n");
             }
         }
     }
-    printf("%d %d %d %d\n",b,c,d,e);
+    for(i=n;i<=(2*n-2);i=i+1){
+        for(j=0;j<=(3*n-i-3);j=j+1){
+            printf("%c",r[i][j]);
+            if(j==(3*n-i-3)){
+                printf("\n");
+            }
+        }
+    }
     return 0;
 }
